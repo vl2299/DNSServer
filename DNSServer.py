b43de2395b92a6d02c28086e78fca1c5278b60a9
vianalin13 <lin.viana13@gmail.com>
2025-03-22 03:09:15 -0400

    trying to make test exfiltrate work

--- DNSServer.py
+++ DNSServer.py
@@ -43,7 +43,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = b"Tandon" # Remember it should be a byte-object
+salt = b'Tandon' # Remember it should be a byte-object
 password = "vl2299@nyu.edu"
 input_string = "AlwaysWatching"
 
@@ -75,33 +75,39 @@ dns_records = {
             86400, #minimum
         ),
     },
-   
-    # Add more records as needed (see assignment instructions!
-    "safebank.com." : {dns.rdatatype.A: "192.168.1.102",},
-    "google.com." : {dns.rdatatype.A: "192.168.1.103",},
-    "legitsite.com." : {dns.rdatatype.A: "192.168.1.104",},
-    "yahoo.com.": {dns.rdatatype.A: "192.168.1.105",},
-    "nyu.edu.": {
-        dns.rdatatype.A: "192.168.1.106",
-        dns.rdatatype.TXT: (encrypted_value.decode("utf-8"),),
-        dns.rdatatype.MX: [(10, "mxa-00256a01.gslb.pphosted.com.")],
-        dns.rdatatype.AAAA: "2001:0db8:85a3:0000:0000:8a2e:0373:7312",
-        dns.rdatatype.NS: "ns1.nyu.edu"
-    }
+    'safebank.com.': {
+        dns.rdatatype.A: '192.168.1.102',
+    },
+    'google.com.': {
+        dns.rdatatype.A: '192.168.1.103',
+    },
+    'legitsite.com.': {
+        dns.rdatatype.A: '192.168.1.104',
+    },
+    'yahoo.com.': {
+        dns.rdatatype.A: '192.168.1.105',
+    },
+    'nyu.edu.': {
+        dns.rdatatype.A: '192.168.1.106',
+        dns.rdatatype.TXT: (str(encrypted_value),),
+        dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
+        dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
+        dns.rdatatype.NS: 'ns1.nyu.edu.',
+    },
 }
 
 def run_dns_server():
     # Create a UDP socket and bind it to the local IP address (what unique IP address is used here, similar to webserver lab) and port (the standard port for DNS)
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Research this
-    server_socket.bind(("127.0.0.1", 53))
+    server_socket.bind(('0.0.0.0', 53))
 
     while True:
         try:
             # Wait for incoming DNS requests
             data, addr = server_socket.recvfrom(1024)
-            # Parse the request using the `dns.message.from_wire` method
+            # Parse the request using the dns.message.from_wire method
             request = dns.message.from_wire(data)
-            # Create a response message using the `dns.message.make_response` method
+            # Create a response message using the dns.message.make_response method
             response = dns.message.make_response(request)
 
             # Get the question from the request
@@ -109,9 +115,9 @@ def run_dns_server():
             qname = question.name.to_text()
             qtype = question.rdtype
 
-            # Check if there is a record in the `dns_records` dictionary that matches the question
+            # Check if there is a record in the dns_records dictionary that matches the question
             if qname in dns_records and qtype in dns_records[qname]:
-                # Retrieve the data for the record and create an appropriate `rdata` object for it
+                # Retrieve the data for the record and create an appropriate rdata object for it
                 answer_data = dns_records[qname][qtype]
 
                 rdata_list = []
@@ -135,9 +141,9 @@ def run_dns_server():
             # Set the response flags
             response.flags |= 1 << 10
 
-            # Send the response back to the client using the `server_socket.sendto` method and put the response to_wire(), return to the addr you received from
+            # Send the response back to the client using the server_socket.sendto method and put the response to_wire(), return to the addr you received from
             print("Responding to request:", qname)
-            server_socket.sendto(response.to_wire(), addr) 
+            server_socket.sendto(response.to_wire(), addr)
         except KeyboardInterrupt:
             print('\nExiting...')
             server_socket.close()
