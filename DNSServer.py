64cb2ff1e7dfdf1a3ab0cea54ba43ae28ceb5c16
vianalin13 <lin.viana13@gmail.com>
2025-03-22 02:54:02 -0400

    .

--- DNSServer.py
+++ DNSServer.py
@@ -77,8 +77,8 @@ dns_records = {
     },
    
     # Add more records as needed (see assignment instructions!
-    "safebank.com" : {dns.rdatatype.A: "192.168.1.102"},
-    "google.com" : {dns.rdatatype.A: "192.168.1.103"},
+    "safebank.com." : {dns.rdatatype.A: "192.168.1.102"},
+    "google.com." : {dns.rdatatype.A: "192.168.1.103"},
     "legitsite.com" : {dns.rdatatype.A: "192.168.1.104"},
     "yahoo.com": {dns.rdatatype.A: "192.168.1.105"},
     "nyu.edu.": {
